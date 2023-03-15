2ec934e5c74a3bf5b3aeeafcfd7c58c2838feeda
Minsup Lee <m1nsuppp@gmail.com>
2023-03-15 23:06:48 +0900

    test: example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello UI LAB!</h1>;
+  return (
+    <h1 role={'heading'} className="text-3xl font-bold underline">
+      Hello UI LAB!
+    </h1>
+  );
 }
