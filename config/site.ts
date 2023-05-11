1d60508b9b6e5fd8924e974d712a7b92c9f19752
Minsup Lee <m1nsuppp@gmail.com>
2023-05-12 00:06:25 +0900

    feat: `SEO` 데이터 수정

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ import { SiteConfig } from '@/types';
 export const siteConfig: SiteConfig = {
   name: `m1nsuppp's blog`,
   author: 'm1nsuppp',
-  description: '웹 개발에 대해 다루는 블로그입니다.',
-  ogImage: 'https://m1nsuppp.site/og.png',
+  description: '웹 개발에 대해 다루는 블로그',
+  ogImage: 'https://m1nsuppp.site/og.webp',
   url: 'https://m1nsuppp.site',
 };
