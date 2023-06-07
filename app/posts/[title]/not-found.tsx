0e72e1685516ab72481038def88e92a322f74ef0
Minsup Lee <m1nsuppp@gmail.com>
2023-06-07 23:57:20 +0900

    feat: 뒤로가기 버튼 추가

--- app/posts/[title]/not-found.tsx
+++ app/posts/[title]/not-found.tsx
@@ -1,19 +1,25 @@
-import Link from 'next/link';
+'use client';
+
+import { useRouter } from 'next/navigation';
 
 export default function NotFoundPage() {
+  const { back } = useRouter();
+
+  const handleGoBackButtonClick = () => back();
+
   return (
     <main className="mt-20 w-full flex flex-col justify-center items-center gap-y-10">
       <h1 className="text-white sm:text-4xl text-2xl">
         존재하지 않는 페이지입니다.
       </h1>
-      <Link
-        href="/"
+      <button
         className="text-sm font-medium text-red-400 group active:text-red-500 focus:outline-none focus:ring"
+        onClick={handleGoBackButtonClick}
       >
         <span className="relative block px-8 py-3 bg-slate-700/95 border border-current">
-          홈으로 이동
+          돌아가기
         </span>
-      </Link>
+      </button>
     </main>
   );
 }
