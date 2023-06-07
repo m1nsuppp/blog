d289d633baf56201a8697a031a7f19c15028c1bc
Minsup Lee <m1nsuppp@gmail.com>
2023-06-08 00:32:04 +0900

    refactor: 디렉토리 구조 변경

---
+++ components/post/post-card.tsx
@@ -0,0 +1,24 @@
+import { Post } from '@/.contentlayer/generated';
+import Link from 'next/link';
+
+type PostCardProps = {
+  post: Post;
+};
+
+export default function PostCard({ post }: PostCardProps) {
+  return (
+    <article className="mt-12 text-white">
+      <Link href={post.url}>
+        <h2 className="font-bold text-xl tracking-tight mb-4 hover:text-red-400">
+          {post.title}
+        </h2>
+        <p className="text-base tracking-tight">
+          {post.description}
+          <span className="font-bold block underline whitespace-nowrap mt-2">
+            더 읽기
+          </span>
+        </p>
+      </Link>
+    </article>
+  );
+}
