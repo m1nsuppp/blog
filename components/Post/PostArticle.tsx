853672a002998e51fc7b50f473cd237f4c2fb60e
Minsup Lee <m1nsuppp@gmail.com>
2023-08-28 23:32:43 +0900

    feat: 다크 모드 적용

--- components/Post/PostArticle.tsx
+++ components/Post/PostArticle.tsx
@@ -9,7 +9,7 @@ const PostArticle = (post: Post) => {
   const { title } = post;
 
   return (
-    <article className="max-w-4xl mx-auto py-12 px-4 prose">
+    <article className="max-w-4xl mx-auto py-12 px-4 prose dark:prose-invert">
       <h1
         className={cn(
           'max-w-4xl font-bold md:font-black',
@@ -21,21 +21,16 @@ const PostArticle = (post: Post) => {
       </h1>
       <MDXComponent
         components={{
-          h1: (h1) => <h1 className="text-white">{h1.children}</h1>,
-          h2: (h2) => <h2 className="text-white">{h2.children}</h2>,
-          h3: (h3) => <h3 className="text-white">{h3.children}</h3>,
-          p: (p) => <p className="text-white">{p.children}</p>,
           code: (code) => <code className="text-red-400">{code.children}</code>,
-          strong: (strong) => <strong className="text-zinc-200">{strong.children}</strong>,
+          strong: (strong) => <strong className="text-zinc-300">{strong.children}</strong>,
           a: (a) => (
             <a
-              className="text-blue-400"
+              className="text-blue-400 underline"
               href={a.href}
             >
               {a.children}
             </a>
           ),
-          li: (li) => <li className="text-gray-300">{li.children}</li>,
         }}
       />
     </article>
