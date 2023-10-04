8155801ef7f5c4be2be635ee9f2aad2235175faa
Minsup Lee <m1nsuppp@gmail.com>
2023-10-04 22:17:22 +0900

    feat: `a11y`를 고려하여 heading 변경

--- components/post/post-card.tsx
+++ components/post/post-card.tsx
@@ -3,13 +3,15 @@ import Link from 'next/link';
 
 const PostCard = (post: Post) => {
   return (
-    <Link
-      href={post.url}
-      className="hover:bg-zinc-800 group p-4 rounded"
-    >
-      <h3 className="text-2xl tracking-tight group-hover:text-red-400 mb-3">{post.title}</h3>
-      <p>{post.description}</p>
-    </Link>
+    <li role="article">
+      <Link
+        href={post.url}
+        className="hover:bg-zinc-800 group p-4 rounded"
+      >
+        <h2 className="text-2xl tracking-tight group-hover:text-red-400 mb-3">{post.title}</h2>
+        <p>{post.description}</p>
+      </Link>
+    </li>
   );
 };
 
